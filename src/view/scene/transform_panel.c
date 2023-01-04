b4aa7470d5f8eab31b28563579c42eec2fdf661d
myregree <kadyr.atakhanov@gmail.com>
2023-01-04 20:22:16 +0300

    Added scale transform;

--- src/view/scene/transform_panel.c
+++ src/view/scene/transform_panel.c
@@ -13,6 +13,12 @@ void handleTransformPanelTabPressed(App *app) {
     } else if (app->ui.currentInputText == TRANSFORM_ROTATION_Y) {
       app->ui.currentInputText = TRANSFORM_ROTATION_Z;
     } else if (app->ui.currentInputText == TRANSFORM_ROTATION_Z) {
+      app->ui.currentInputText = TRANSFORM_SCALE_X;
+    } else if (app->ui.currentInputText == TRANSFORM_SCALE_X) {
+      app->ui.currentInputText = TRANSFORM_SCALE_Y;
+    } else if (app->ui.currentInputText == TRANSFORM_SCALE_Y) {
+      app->ui.currentInputText = TRANSFORM_SCALE_Z;
+    } else if (app->ui.currentInputText == TRANSFORM_SCALE_Z) {
       app->ui.currentInputText = TRANSFORM_POSITION_X;
     } else {
       app->ui.currentInputText = TRANSFORM_POSITION_X;
@@ -23,7 +29,6 @@ void handleTransformPanelTabPressed(App *app) {
 void UpdateTransformPanel(App *app) {
   // handle tab pressed
   handleTransformPanelTabPressed(app);
-
   // position
   HandleInputText(&app->ui.transform_position_x, &app->ui.currentInputText);
   HandleInputText(&app->ui.transform_position_y, &app->ui.currentInputText);
@@ -31,7 +36,6 @@ void UpdateTransformPanel(App *app) {
   HandleKeys(app, &app->ui.transform_position_x, app->ui.currentInputText);
   HandleKeys(app, &app->ui.transform_position_y, app->ui.currentInputText);
   HandleKeys(app, &app->ui.transform_position_z, app->ui.currentInputText);
-
   // rotation
   HandleInputText(&app->ui.transform_rotation_x, &app->ui.currentInputText);
   HandleInputText(&app->ui.transform_rotation_y, &app->ui.currentInputText);
@@ -39,6 +43,13 @@ void UpdateTransformPanel(App *app) {
   HandleKeys(app, &app->ui.transform_rotation_x, app->ui.currentInputText);
   HandleKeys(app, &app->ui.transform_rotation_y, app->ui.currentInputText);
   HandleKeys(app, &app->ui.transform_rotation_z, app->ui.currentInputText);
+  // scale
+  HandleInputText(&app->ui.transform_scale_x, &app->ui.currentInputText);
+  HandleInputText(&app->ui.transform_scale_y, &app->ui.currentInputText);
+  HandleInputText(&app->ui.transform_scale_z, &app->ui.currentInputText);
+  HandleKeys(app, &app->ui.transform_scale_x, app->ui.currentInputText);
+  HandleKeys(app, &app->ui.transform_scale_y, app->ui.currentInputText);
+  HandleKeys(app, &app->ui.transform_scale_z, app->ui.currentInputText);
 }
 
 void drawTransformPosition(App *app, int left, int top) {
@@ -55,6 +66,13 @@ void drawTransformRotation(App *app, int left, int top) {
   DrawInputText(&app->ui.transform_rotation_z, &app->ui.currentInputText);
 }
 
+void drawTransformScale(App *app, int left, int top) {
+  DrawText("Scale", left + 15, top + 25, 14, Fade(BLACK, 0.5f));
+  DrawInputText(&app->ui.transform_scale_x, &app->ui.currentInputText);
+  DrawInputText(&app->ui.transform_scale_y, &app->ui.currentInputText);
+  DrawInputText(&app->ui.transform_scale_z, &app->ui.currentInputText);
+}
+
 void DrawTransformPanel(App *app) {
   int width = TRANSFORM_PANEL_WIDTH;
   int height = TRANSFORM_PANEL_HEIGHT;
@@ -65,6 +83,7 @@ void DrawTransformPanel(App *app) {
   DrawText("TRANSFORM", left + 5, top + 5, 14, BLACK);
   drawTransformPosition(app, left, top);
   drawTransformRotation(app, left, top + 50);
+  drawTransformScale(app, left, top + 100);
 }
 
 void initTransformPosition(App *app) {
@@ -125,7 +144,37 @@ void initTransformRotation(App *app) {
   ); 
 }
 
+void initTransformScale(App *app) {
+  int inputTextWidth = 100;
+  int inputTextTop = 165;
+
+  InitInputText(
+    &app->ui.transform_scale_x,
+    TRANSFORM_SCALE_X,
+    DOUBLE_NUMBER_INPUT,
+    (Rectangle) { GetScreenWidth() - TRANSFORM_PANEL_WIDTH + 5, inputTextTop, inputTextWidth, 20 },
+    "x", "0"
+  );
+
+  InitInputText(
+    &app->ui.transform_scale_y,
+    TRANSFORM_SCALE_Y,
+    DOUBLE_NUMBER_INPUT,
+    (Rectangle) { GetScreenWidth() - (TRANSFORM_PANEL_WIDTH - inputTextWidth) + 30, inputTextTop, inputTextWidth, 20 },
+    "y", "0"
+  );
+
+  InitInputText(
+    &app->ui.transform_scale_z,
+    TRANSFORM_SCALE_Z,
+    DOUBLE_NUMBER_INPUT,
+    (Rectangle) { GetScreenWidth() - (TRANSFORM_PANEL_WIDTH - (inputTextWidth * 2)) + 55, inputTextTop, inputTextWidth, 20 },
+    "z", "0"
+  ); 
+}
+
 void InitTransformPanel(App *app) {
   initTransformPosition(app);
   initTransformRotation(app);
+  initTransformScale(app);
 }
