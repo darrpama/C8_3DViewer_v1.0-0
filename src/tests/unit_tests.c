650790904cfa6045791df9668e47718d12bf97ec
Williamc Commando <williamc@student.21-school.ru>
2023-01-09 19:29:31 +0300

    deleted an unnecessary test

--- src/tests/unit_tests.c
+++ src/tests/unit_tests.c
@@ -36,18 +36,12 @@ START_TEST(test01) {
 }
 END_TEST
 
-START_TEST(test02) {
-    ck_assert_int_eq(1, 2);
-}
-END_TEST
-
 int main(void) {
     Suite *s1 = suite_create("Core");
     TCase *tc1 = tcase_create("Core");
     SRunner *sr = srunner_create(s1);
     suite_add_tcase(s1, tc1);
     tcase_add_test(tc1, test01);
-    tcase_add_test(tc1, test02);
 
     srunner_run_all(sr, CK_ENV);
     srunner_free(sr);
